2303713ebb94bb907b559bbed9089a43ea91edb0
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2021-07-17 02:23:10 +0200

    fix date tests

--- serial.test.js
+++ serial.test.js
@@ -1,4 +1,12 @@
 const serial = require("./index.js");
+const date = new Date();
+const [month, day, year] = [
+  date.toLocaleString("en-us", { month: "2-digit" }),
+  date.toLocaleString("en-us", { day: "2-digit" }),
+  date.getFullYear(),
+];
+
+const fullDate = year + month + day;
 
 test("current serial has 10 characters", () => {
   expect(String(serial()).length).toBe(10);
@@ -9,14 +17,14 @@ test("custom serial has 10 characters", () => {
 });
 
 test("correct added change number", () => {
-  expect(String(serial(2021071500))).toBe("2021071501");
+  expect(String(serial(fullDate + "00"))).toBe(`${fullDate}01`);
 });
 
 test("correct add zero", () => {
-  expect(String(serial(202107150))).toBe("2021071501");
-  expect(String(serial(2021071510))).toBe("2021071511");
+  expect(String(serial(fullDate + "0"))).toBe(`${fullDate}01`);
+  expect(String(serial(fullDate + "10"))).toBe(`${fullDate}11`);
 });
 
 test("new day", () => {
-  expect(String(serial(2021071401))).toBe("2021071500");
+  expect(String(serial(2021071401))).toBe(`${fullDate}00`);
 });
